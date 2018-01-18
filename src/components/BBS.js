16c13008d6e58fe184d1ea9e11fff8ff81b73ac0
stared329 <jamie.kim.0329@gmail.com>
2018-01-18 14:27:37 +0900

    초기화 파일 정리

---
+++ src/components/BBS.js
@@ -0,0 +1,9 @@
+import React, {Component} from 'react';
+
+export default class BBS extends Component {
+    render() {
+      return (
+        <div>BBS</div>
+      )
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import BBS from './components/BBS';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<BBS />, document.getElementById('root'));
 registerServiceWorker();
