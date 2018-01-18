787d6d9969c507463a08a25e26f75894edacb5b6
stared329 <jamie.kim.0329@gmail.com>
2018-01-18 14:39:44 +0900

    firebase 설치

--- src/components/BBS.js
+++ src/components/BBS.js
@@ -1,6 +1,19 @@
 import React, {Component} from 'react';
+import * as firebase from 'firebase';
 
 export default class BBS extends Component {
+  //lifecycle hook
+  componentDidMount() {
+    const config = {
+      apiKey: "AIzaSyBByZ9TvPx7pOahDbGl4lkS7Apx1lV1z3Y",
+      authDomain: "fds-cra-bbs.firebaseapp.com",
+      databaseURL: "https://fds-cra-bbs.firebaseio.com",
+      projectId: "fds-cra-bbs",
+      storageBucket: "",
+      messagingSenderId: "281741899963"
+    };
+    firebase.initializeApp(config);
+  }
     render() {
       return (
         <div>BBS</div>
